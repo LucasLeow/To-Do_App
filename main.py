6f3e701ef0bd4c5ddf319d69a1a2411c7bfcb248
Lucas Leow <leowjianhoong@gmail.com>
2023-09-17 10:45:36 +0800

    completed todo router

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 import models
 from database import engine
-from routers import auth
+from routers import auth, todos
 
 app = FastAPI()
 
 app.include_router(auth.router)
+app.include_router(todos.router)
 
 
