37d0f8128d7abf2998c767c7c7b120666317fcbf
Lucas Leow <leowjianhoong@gmail.com>
2023-09-17 14:23:32 +0800

    completed user router

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from routers import auth, todos, admin
+from routers import auth, todos, admin, users
 
 app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
-
+app.include_router(users.router)
