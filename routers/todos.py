91d76ccf864b7e7a100d1f99ee2d1529ab34e376
Lucas Leow <leowjianhoong@gmail.com>
2023-09-21 20:53:35 +0800

    implemented adding todos frontend + backend

--- routers/todos.py
+++ routers/todos.py
@@ -4,11 +4,12 @@ from typing import Annotated
 from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 
-from fastapi import APIRouter, Depends, Path, HTTPException, Request
+from fastapi import APIRouter, Depends, Path, HTTPException, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 from starlette import status
+from starlette.responses import RedirectResponse
 
 sys.path.append('../FullStackToDoApp')
 from persistence.models import *
@@ -40,6 +41,24 @@ async def read_all_todo_by_user(request: Request, db: db_dependency):
 @router.get("/add-todo", response_class=HTMLResponse)
 async def add_new_todo(request: Request):
     return templates.TemplateResponse("add-todo.html", {'request': request})
+
+# path must be same name as html name for form (add-todo.html)
+@router.post("/add-todo", response_class=HTMLResponse)
+async def create_todo( db: db_dependency, request: Request, title: str = Form(...), description: str = Form(...),
+                      priority: int = Form(...)):
+    todo_model = Todos()
+    todo_model.title = title
+    todo_model.description = description
+    todo_model.priority = priority
+    todo_model.complete = False
+    todo_model.owner_id = 1 # implement dynamically with Auth later
+
+    db.add(todo_model)
+    db.commit()
+
+    return RedirectResponse(url='/todos', status_code=status.HTTP_302_FOUND)
+
+
 
 @router.get("/edit-todo/{todo_id}", response_class=HTMLResponse)
 async def edit_todo(request: Request):
