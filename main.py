4780a249448fe2cf8b770f7d381a6ed7d7dcbb38
Lucas Leow <leowjianhoong@gmail.com>
2023-09-22 16:14:05 +0800

    refactored code

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 from routers import auth, todos, admin, users
 from starlette.staticfiles import StaticFiles
+from starlette import status
+from starlette.responses import RedirectResponse
 
 app = FastAPI()
 
 app.mount('/static', StaticFiles(directory='static'), name='static')
+
+@app.get('/')
+async def root():
+    return RedirectResponse(url='/todos', status_code=status.HTTP_302_FOUND)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
--- routers/auth.py
+++ routers/auth.py
@@ -23,13 +23,6 @@ from fastapi.templating import Jinja2Templates
 SECRET_KEY = "76914b3e3a0bafeb34b3024f0f014bb85b5755127f22d6e5e9a7a05f45f5eb84"
 ALGO = 'HS256'
 
-class CreateUserRequest(BaseModel):
-    username: str
-    email: Optional[str]
-    first_name: str
-    last_name: str
-    password: str
-
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 models.Base.metadata.create_all(bind=database.engine)
@@ -101,27 +94,11 @@ async def get_current_user(request: Request):
         username: str = payload.get('username')
         user_id: int = payload.get('id')
         if username is None or user_id is None:
-            return None
+            logout(request)
         return {'username': username, 'id': user_id}
     except JWTError:
-        raise get_user_exception()
+        raise HTTPException(status_code=404, detailts='Not Found')
 
-
-@router.post('/create/user')
-async def create_new_user(create_user: CreateUserRequest, db: db_dependency):
-    create_user_model = models.Users()
-    create_user_model.email = create_user.email
-    create_user_model.username = create_user.username
-    create_user_model.first_name = create_user.first_name
-    create_user_model.last_name = create_user.last_name
-
-    hash_password = get_password_hash(create_user.password)
-
-    create_user_model.hashed_password = hash_password
-    create_user_model.is_active = True
-
-    db.add(create_user_model)
-    db.commit()
 
 @router.post('/token')
 async def login_for_access_token(
@@ -204,21 +181,5 @@ async def register_user(db: db_dependency, request: Request,
 
     msg = 'user successfully created'
     return templates.TemplateResponse('login.html', {'request': request, 'msg': msg})
-
-def get_user_exception():
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    return credentials_exception
-
-def token_exception():
-    token_exception_response = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Incorrect username or password",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    return token_exception_response
 
 
